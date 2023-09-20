bf8df21d59530d1179f18a14c5db2d488360e29b
GenovevaCastro <impresionesveva@gmail.com>
2023-09-20 17:02:54 -0600

    Se agrega react con vite y changelog

---
+++ MovieChallenge/src/main.jsx
@@ -0,0 +1,12 @@
+//putno de entrada de nuestra aplicación 
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+//interacción con elementos html
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+<h1><center>V-VA Films</center></h1>
+
+)
