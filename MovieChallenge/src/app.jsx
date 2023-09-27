8eaecb0db22ba51a379f7c84b42090f4426d52c6
GenovevaCastro <impresionesveva@gmail.com>
2023-09-26 20:29:30 -0600

    Se agrega componente footer

--- MovieChallenge/src/app.jsx
+++ MovieChallenge/src/app.jsx
@@ -3,6 +3,7 @@ import Header from './componentes/header/header';
 import Buscador from './componentes/Buscador/Buscador';
 import ContenedorPeli from './componentes/ContenedorPeli/ContenedorPeli';
 import ButtonVerMas from './componentes/BotonVerMas/BotonVerMas';
+import Footer from './componentes/Footer/Footer';
 
 export function App (){
     return(
@@ -11,6 +12,7 @@ export function App (){
             <Buscador/>
             <ContenedorPeli/>
             <ButtonVerMas/>
+            <Footer/>
         </div>
     )
 }
---
+++ MovieChallenge/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <center>
+            <img src="src/tmbd.jpg" alt="imgLogoTmbd"/>
+           <p>
+            Design by Genoveva Castro - Derechos reservados TMBD
+           </p>
+           </center>
+            </footer>
+    )
+}
+
+export default Footer
