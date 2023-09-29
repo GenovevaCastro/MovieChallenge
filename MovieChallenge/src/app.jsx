6bfb89d3cbb2d7a5dd62f47ea577d6a4cc4fcd0e
GenovevaCastro <impresionesveva@gmail.com>
2023-09-28 22:28:14 -0600

    Se agrega funcion Router para abrir la ventana de Home o InfoPelicula

--- MovieChallenge/src/app.jsx
+++ MovieChallenge/src/app.jsx
@@ -3,13 +3,25 @@ import InfoPelicula from './InfoPelicula'
 
  export function App (){
 
-    const location = window.location
+//constante para window location pathname 
+    const location = window.location.pathname
     console.log(location);
 
+    const Router = () =>{
+        //si location se encuentra en "/" abre el Home
+        if(location === "/"){
+            return <Home/>
+            //de lo contrario retorname InfoPelicula
+        }else{
+            return <InfoPelicula/>
+        }
+    }
+
     return(
-            <Home/>,
-            <InfoPelicula/>
-            
+         <>
+          {Router()}
+         </>
+       
     )
 }
 
