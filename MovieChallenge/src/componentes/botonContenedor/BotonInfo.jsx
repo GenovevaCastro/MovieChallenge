37a0aa1ba57361a647271d2bf1e36b1b0c6c7c92
GenovevaCastro <impresionesveva@gmail.com>
2023-09-29 17:55:20 -0600

    Se coloca ruta en botoInfo  para que al dar click envie a la pantalla de InfoPelicula

--- MovieChallenge/src/componentes/botonContenedor/BotonInfo.jsx
+++ MovieChallenge/src/componentes/botonContenedor/BotonInfo.jsx
@@ -1,12 +1,13 @@
 import './BotonInfo.css'
+import { Link } from 'react-router-dom'
 
 function BotonInfo() {
     return (
         <center>
             <div className="botonContenedor">
-            <a href="ventanaEmergente.html">
+            <Link to="/InfoPelicula">
             <button>Información</button>
-            </a>
+            </Link>
             </div>
         </center>
     )
